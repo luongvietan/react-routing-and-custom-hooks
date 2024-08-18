575789b9aa87008b6a809571a9abb387853407ad
Viet An <luongvietan.231123@gmail.com>
2024-08-18 23:58:51 +0700

    Link

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import { Skeleton } from "@mui/material";
 import './App.css'
 import RecipeList from "./pages/recipes";
 import CommentList from "./pages/comments";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate, Link } from "react-router-dom";
 
 function App() {
     const navigate = useNavigate()
     return (
         <div>
+            <div>
+                <Link to={"/recipe-list"}>Alternative way of navigating to recipe list page</Link>
+            </div>
             <button onClick={()=>{navigate("/recipe-list")}} style={{background : 'black', color : 'white', margin : '10px'}}>Recipe List Page</button>
             <button onClick={()=>{navigate("/comment-list")}} style={{ background: 'black', color: 'white', margin : '10px'}}>Comment List Page</button>
             <Routes>
