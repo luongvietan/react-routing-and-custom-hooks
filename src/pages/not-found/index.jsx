113519e6299f7d5501158ffc218ac654416f4f75
Viet An <luongvietan.231123@gmail.com>
2024-08-19 13:43:30 +0700

    Update Routes

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 export default function NotFound() {
     return (
         <div>
-            <h1>Not Found Page</h1>
-            <Link to={'/recipe-list'}>Go to recipe list</Link>
+            <h1>This Page Doesn't Exist</h1>
+            <Link to={'/home'}>Go to Home page</Link>
         </div>
     )
 }
--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -2,8 +2,6 @@ import { useLocation } from "react-router-dom"
 
 export default function RecipeList() {
     const location = useLocation()
-    console.log(location);
-    
     return (
         <div>
             <h1>Recipe List Page</h1>
